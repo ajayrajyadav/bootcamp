713614c3bd1c2c330d74e22e460f0933c1be40a0
Ajay Yadav <ajay.yadav@awksosx014.lan>
2019-02-02 22:23:50 -0800

    start of module-10

---
+++ PREWORK_ARY/Module-10/javascript.js
@@ -0,0 +1,26 @@
+
+document.getElementById("button1").addEventListener("click", function () {
+
+    document.getElementById("box").style.height = "250px";
+
+});
+
+document.getElementById("button2").addEventListener("click", function () {
+
+    document.getElementById("box").style.color = "blue";
+
+});
+
+document.getElementById("colorFade").addEventListener("click", function () {
+
+    document.getElementById("box").style.height = "25px";
+
+});
+
+document.getElementById("button4").addEventListener("click", function () {
+
+    document.getElementById("box").style.height = "150px";
+    document.getElementById("box").style.width = "150px";
+    document.getElementById("box").style.background.color = "orange";
+
+});
