502d27595447112df47c98e2a9d38c585680a814
Ajay Yadav <ajay.yadav@AWKSOSX014.local>
2019-02-06 15:43:30 -0800

    Module-7 complete

--- PREWORK_ARY/Module-7/Broken_and_Working_Code/BrokenCode/logic.js
+++ PREWORK_ARY/Module-7/Broken_and_Working_Code/BrokenCode/logic.js
@@ -5,23 +5,32 @@ $("#factButton").on("click", function() {
 
 var booFacts = ["Boo is a pomeranian", "Boo's best friend is another pomeranian named Buddy", "Boo the Pomeranian was born on March 16", "making him a Pisces", "Boo's favourite food is grass", "Boo has released two books"]
 
+// When the textPink button is pressed...
 $("#textPink").on("click", function() {
-	$("#funText").css("color", pink)
+	// Change funText to pink.
+	$("#funText").css("color", "pink")
 })
 
+// When the textOrange button is pressed...
 $("#textOrange").on("click", function() {
-$("#funText").css("color", "orange")
+	// Change funText to orange.
+	$("#funText").css("color", "orange")
 })
+
+// When the textGreen button is pressed...
 $("#textGreen").on("click", function() {
+	// Change funText to green.
 	$("#funText").css("color", "green")
 })
 
-
-$("#boxGrow").on(click, function() {
-	$("#box").animate({height:"+=35px", 
-		width:"+=35px"}, "fast");
+// When the boxGrow button is clicked... 
+$("#boxGrow").on("click", function() {
+	// Increase the size of the box.
+	$("#box").animate({height:"+=35px", width:"+=35px"}, "fast");
 })
 
-$("#boxShrink").on(click, function() {
+// When the boxShrink button is clicked...
+$("#boxShrink").on("click", function() {
+	// Decrease the size of the box.
 	$("#box").animate({height:"-=35px", width:"-=35px"}, "fast");
 })
