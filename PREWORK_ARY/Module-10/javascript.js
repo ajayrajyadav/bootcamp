ff7a694b2ea58e4e7bc20e43f86b286e1882df25
Ajay Yadav <ajay.yadav@AWKSOSX014.local>
2019-02-07 15:48:21 -0800

    Module-10 upgrade

--- PREWORK_ARY/Module-10/javascript.js
+++ PREWORK_ARY/Module-10/javascript.js
@@ -1,9 +1,14 @@
 
 document.getElementById("button1").addEventListener("click", function () {
-
-    document.getElementById("box").style.height = "250px";
-    document.getElementById("box").style.opacity = "1";
-
+    var elem = document.getElementById("box");
+    var height = window.getComputedStyle(elem,null).getPropertyValue("height");
+    var color = window.getComputedStyle(elem,null).getPropertyPriority("color")
+    height = height.replace("px","");
+    height= +height;
+    var n = height;
+    height = n+50+'px';
+    document.getElementById("box").style.height = height;
+    document.getElementById("box").style.color = color;
 });
 
 document.getElementById("button2").addEventListener("click", function () {
