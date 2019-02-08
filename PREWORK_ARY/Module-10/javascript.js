f1ba3372e31ec0de5cd0cb64ee09af7cceb0192f
AJAY YADAV <ajay@ajays-mbp.lan>
2019-02-07 22:11:35 -0800

    filename changes

--- PREWORK_ARY/Module-10/javascript.js
+++ PREWORK_ARY/Module-10/javascript.js
@@ -2,13 +2,11 @@
 document.getElementById("button1").addEventListener("click", function () {
     var elem = document.getElementById("box");
     var height = window.getComputedStyle(elem,null).getPropertyValue("height");
-    var color = window.getComputedStyle(elem,null).getPropertyPriority("color")
     height = height.replace("px","");
     height= +height;
     var n = height;
     height = n+50+'px';
     document.getElementById("box").style.height = height;
-    document.getElementById("box").style.color = color;
 });
 
 document.getElementById("button2").addEventListener("click", function () {
